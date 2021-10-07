2759dc4a9442a5ce76bf459ec5a46fecba02c10c
Aleksandar Milosavljevic <milosavljevic246@gmail.com>
2021-10-07 19:56:49 +0200

    Add new command and update parameters

--- Modules/cd-fzf/cd-fzf.psm1
+++ Modules/cd-fzf/cd-fzf.psm1
@@ -1,15 +1,26 @@
 
-function Set-FzfLocation {
+function Get-FzfLocation {
     param(
-        [Parameter(Mandatory = $false)]
-        [string]
-        $DirName = ""
+        [Parameter(Mandatory = $false)] [string] $Filter = "",
+        [Parameter(Mandatory = $false)] [string] $Path = ".",
+        [Parameter(Mandatory = $false)] [int] $Depth = 10,
+        [switch] $Directory
     )
 
-    Get-ChildItem -Recurse -Directory -Filter $DirName |
+    Get-ChildItem -Recurse -Directory:$Directory -Path $Path -Filter $Filter -Depth $Depth |
      Select-Object FullName -ExpandProperty FullName |
-      fzf |
-      Set-Location
+      fzf
+}
+
+function Set-FzfLocation {
+    param(
+        [Parameter(Mandatory = $false)] [string] $Filter = "",
+        [Parameter(Mandatory = $false)] [string] $Path = ".",
+        [Parameter(Mandatory = $false)] [int] $Depth = 10,
+        [switch] $Directory
+    )
+
+    Get-FzfLocation -Directory:$Directory -Filter $Filter -Depth $Depth | Set-Location
 }
 
 Set-Alias -Name cd-fzf -Value Set-FzfLocation
