c77aca387e254c723194ccc8cffe3b20bf02a905
Aleksandar Milosavljevic <milosavljevic246@gmail.com>
2021-10-07 20:02:33 +0200

    Remove unecessery parameter

--- Modules/fzf-cd/fzf-cd.psm1
+++ Modules/fzf-cd/fzf-cd.psm1
@@ -16,11 +16,10 @@ function Set-FzfLocation {
     param(
         [Parameter(Mandatory = $false)] [string] $Filter = "",
         [Parameter(Mandatory = $false)] [string] $Path = ".",
-        [Parameter(Mandatory = $false)] [int] $Depth = 10,
-        [switch] $Directory
+        [Parameter(Mandatory = $false)] [int] $Depth = 10
     )
 
-    Get-FzfLocation -Directory:$Directory -Filter $Filter -Depth $Depth | Set-Location
+    Get-FzfLocation -Directory -Filter $Filter -Depth $Depth | Set-Location
 }
 
 Set-Alias -Name fzf-cd -Value Set-FzfLocation
