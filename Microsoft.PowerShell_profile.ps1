841ddbb6946679a5e47694045d777ee1f26131c9
Aleksandar Milosavljevic <milosavljevic246@gmail.com>
2021-10-22 23:51:38 +0200

    Update entry point

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,17 @@
-﻿
-# Chocolatey profile
+﻿# Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
 
+# Update to latest powershell with 
+# choco upgrade powershell -y
+
 # fish like autosuggestion
 Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineKeyHandler -Chord Ctrl+f -Function ForwardWord
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineKeyHandler -Chord Ctrl-p -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Chord Ctrl-n -Function HistorySearchForward
 
 Set-PoshPrompt -Theme hotstick.minimal
 Set-Alias -Name chtsh -Value Get-ChtSh
