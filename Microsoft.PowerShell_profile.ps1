cf907b969ea301015f7ecbfdbf62ae2c600aaf84
Aleksandar Milosavljevic <milosavljevic246@gmail.com>
2021-10-29 08:49:11 +0200

    Setting aliases for git

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -13,5 +13,23 @@ Set-PSReadLineKeyHandler -Chord Ctrl-p -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Chord Ctrl-n -Function HistorySearchForward
 
 Set-PoshPrompt -Theme wopian
-Set-Alias -Name chtsh -Value Get-ChtSh
 
+Set-Alias -Name chtsh -Value Get-ChtSh
+Set-Alias -Name b -Value Set-FzfBranch
+
+# functions to alias
+function Get-GitStatus { & git status }
+Set-Alias -Name gs -Value Get-GitStatus -Force
+function Get-GitLog {
+    param(
+        [switch] $Diff
+    )
+    if ($Diff) {
+      git log -p $arg
+    } else {
+      git log $arg
+    }
+}
+Set-Alias -Name gl -Value Get-GitLog -Force
+function Get-GitFetch { & git fetch $args }
+New-Alias -Name f -Value Get-GitFetch -Force
